4c6bce13294020a56cf413ea2cf3ef0794b3c362
aphemra <aphemra@gmail.com>
2022-10-20 00:13:42 -0400

    Added makeRequest helper function * Makes calls to axios (newly installed) much easier.

---
+++ client/src/services/makeRequest.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const api = axios.create({
+	baseURL: process.env.REACT_APP_SERVER_URL,
+});
+
+export function makeRequest(url, options) {
+	return api(url, options)
+		.then((response) => response.data)
+		.catch((error) => Promise.reject(error?.response?.data?.message ?? "Client-side Error."));
+}
