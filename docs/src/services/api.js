634ba599886f20527935a79786386ac57be8b354
aphemra <aphemra@gmail.com>
2022-10-23 14:08:22 -0400

    Renamed client folder to docs for ghpages deployment

--- docs/src/services/api.js
+++ docs/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: process.env.REACT_APP_SERVER_URL,
+	baseURL: "https://todo-app-aphemra.vercel.app",
 	headers: {
 		"Content-Type": "application/json",
 	},
